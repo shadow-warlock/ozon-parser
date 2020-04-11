e09102121791e4dfd6148b51dd08387c2015e724
timager <timger98@gmail.com>
2020-04-11 22:47:13 +0300

    database fix

--- dbtest.py
+++ dbtest.py
@@ -1,6 +1,5 @@
 import sqlite3
 
 connect = sqlite3.connect("database.sqlite")  # или :memory: чтобы сохранить в RAM
-connect.cursor().execute("""SELECT * FROM items WHERE id=1""")
-data = connect.cursor().fetchall()
+data = connect.cursor().execute("""SELECT * FROM items""").fetchall()
 print(data)
