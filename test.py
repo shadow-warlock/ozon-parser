cd3d9c8432c92844b3756d9adc804b59cf363cef
timager <timger98@gmail.com>
2020-04-11 16:17:41 +0300

    format

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ options.headless = True
 
 driver = webdriver.Firefox(options=options)
 driver.get("https://www.ozon.ru/context/detail/id/159397500/")
-driver.implicitly_wait(5) # seconds
+driver.implicitly_wait(5)  # seconds
 item_name = driver.find_element_by_css_selector('[data-widget="webProductHeading"]>h1').text
 print(item_name)
 item_score = driver.find_element_by_css_selector(
@@ -37,9 +37,10 @@ button = driver.find_elements_by_css_selector('[qa-id="slider-next"]>button')[1]
 # button.click()
 
 for element in recommends:
-    name = element.find_element_by_css_selector('div:nth-child(3)').get_property("textContent")
+    name = element.find_element_by_css_selector('div:nth-child(3)').get_property("textContent").strip()
     score = element.find_element_by_css_selector('div:nth-child(4)').get_attribute('title')
-    price = element.find_element_by_css_selector('div:nth-child(2)>div:nth-child(1)>span>span>span').get_property("textContent")
+    price = element.find_element_by_css_selector('div:nth-child(2)>div:nth-child(1)>span>span>span').get_property(
+        "textContent").strip()
     print(name + " " + price + " " + score)
 
 driver.get_screenshot_as_file("screen.png")
