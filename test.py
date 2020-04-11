4155d2009e6020a4c39af043d2f9cc07f880b397
timager <timger98@gmail.com>
2020-04-11 16:49:05 +0300

    sponsored

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+
 options = Options()
 options.headless = True
 
@@ -7,24 +8,27 @@ options.headless = True
 def load(url):
     driver = webdriver.Firefox(options=options)
     driver.get(url)
-    driver.implicitly_wait(5)  # seconds
     item_name = driver.find_element_by_css_selector('[data-widget="webProductHeading"]>h1').text
     item_score = driver.find_element_by_css_selector(
         '[data-widget="container"] [data-widget="reviewProductScore"]>div>div>div>div').get_attribute('style').replace(
         "width: ", "").replace(";", '')
     item_price = driver.find_element_by_xpath(
-        '/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/div/div[1]/div/span[1]').text
+        '/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/div/div[1]/div/span[1]').text.replace(
+        ' ', '')
 
     print("Name:" + item_name + " Price:" + item_price + " Score:" + item_score)
     return driver
 
 
 driver = load("https://www.ozon.ru/context/detail/id/159397500/")
-recommends = driver.find_elements_by_css_selector(
-    '[data-widget="skuShelfCompare"]>div>div>div>div>div>div>a')
+recommends = driver.find_elements_by_css_selector('[data-widget="skuShelfCompare"]>div>div>div>div>div>div>a')
 print("---RECOMMENDS---")
 for element in recommends:
     load(element.get_property("href"))
 
-driver.get_screenshot_as_file("screen.png")
+sponsored = driver.find_elements_by_css_selector('[data-widget="skuShelfGoods"]>div>div>div>div>div>a')
+print("---SPONSORED---")
+for element in sponsored:
+    load(element.get_property("href"))
+
 driver.close()
