12258ca743f667c73344e72ab90cd2bfa4389d3a
nik <nik_mak@bk.ru>
2020-04-11 19:56:28 +0300

    update wait and driver close + additional params

--- test.py
+++ test.py
@@ -5,30 +5,41 @@ options = Options()
 options.headless = True
 
 
-def load(url):
+def load(url, wait=None):
+    print(url)
     driver = webdriver.Firefox(options=options)
     driver.get(url)
+    if wait is not None:
+        driver.implicitly_wait(wait)  # seconds
     item_name = driver.find_element_by_css_selector('[data-widget="webProductHeading"]>h1').text
-    item_score = driver.find_element_by_css_selector(
-        '[data-widget="container"] [data-widget="reviewProductScore"]>div>div>div>div').get_attribute('style').replace(
-        "width: ", "").replace(";", '')
+    item_sale = driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[1]/div/div/div[1]/div[1]/span[1]').text
+    item_score = driver.find_element_by_xpath(
+        '/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/div').get_attribute('title')
     item_price = driver.find_element_by_xpath(
         '/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[3]/div[2]/div/div[1]/div/div/div/div/div[1]/div/span[1]').text.replace(
         ' ', '')
-
-    print("Name:" + item_name + " Price:" + item_price + " Score:" + item_score)
+    item_100 = driver.find_elements_by_xpath("/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[3]/div[1]/div[2]/div[3]/div/div/span/span")
+    reviews = driver.find_element_by_xpath("/html/body/div[1]/div/div[1]/div[4]/div[2]/div[2]/div/div[1]/div/div/div[2]/div[1]/div/div/a").text
+    reviews = reviews.split()[0]
+    if len(item_100) != 0:
+        item_100 = "True"
+    else:
+        item_100 = "False"
+    print("Name:" + item_name + " Price:" + item_price + " Score:" + item_score + " Sale:" + item_sale + " 100+:" + item_100 + " Reviews:" + reviews)
     return driver
 
 
-driver = load("https://www.ozon.ru/context/detail/id/159397500/")
+driver = load("https://www.ozon.ru/context/detail/id/159397500/", 5)
 recommends = driver.find_elements_by_css_selector('[data-widget="skuShelfCompare"]>div>div>div>div>div>div>a')
 print("---RECOMMENDS---")
 for element in recommends:
-    load(element.get_property("href"))
+    local_driver = load(element.get_property("href"), 1)
+    local_driver.close()
 
 sponsored = driver.find_elements_by_css_selector('[data-widget="skuShelfGoods"]>div>div>div>div>div>a')
 print("---SPONSORED---")
 for element in sponsored:
-    load(element.get_property("href"))
+    local_driver = load(element.get_property("href"), 1)
+    local_driver.close()
 
 driver.close()
